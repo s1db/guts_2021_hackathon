48950fbbc9047d5bf32f655a090a865319207dd4
Diego Drago <2384810d@student.gla.ac.uk>
2021-02-06 23:29:50 +0100

    half assed Open Graph

--- frontend/components/Layout.jsx
+++ frontend/components/Layout.jsx
@@ -14,6 +14,13 @@ const Layout = ({full = false, children}) => {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
         <meta name="msapplication-TileColor" content="#da532c"/>
         <meta name="theme-color" content="#ffffff"/>
+
+        <meta property="og:title" content="Foodbank" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://foobdbank.com/" />
+        <meta property="og:image" content="https://foobdbank.com/thumbnail.png" />
+        <meta property="og:description" content="Asking for food while in need shouldn't be taboo. Food is a human right. A network of foodbanks to provide support when times are rough." />
+        
       </head>
       <Nav />
       <div className={cn("fit mx-auto", !full && "px-6 max-w-7xl")}>
