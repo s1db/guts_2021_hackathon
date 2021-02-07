c1d76e7c7d8a66e69e8ad6cf5b858cc5192eae5a
Lenka Vincenova <vinlenka@seznam.cz>
2021-02-07 01:32:44 +0000

    Update views.py
    added user views

--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ from datetime import timedelta
 import json
 import random
 import string
-from api.models import Address, CharityAccount, CharityDietaryOptions, DiertaryRequirements, FoodRequest, User
+from api.models import Address, CharityAccount, CharityDietaryOptions, DiertaryRequirements, FoodRequest, User, UserDietaryPreferences
 from django.utils import timezone
 from django.views import View
 from django.core.serializers import serialize
@@ -14,6 +14,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from django.http import JsonResponse, HttpResponse
 from app.settings import EMAIL_HOST_USER
+from django.db.utils import IntegrityError
 
 date_format = '%Y-%m-%d %H:%M:%S'
 
@@ -289,6 +290,259 @@ class charityView(View):
             except Exception as e:
                 print(e)
                 return JsonResponse({"error":"Account does not exist"})
+
+            
+class CreateUserAccountView(View):
+
+    def get(self, request):
+        response = HttpResponse(content_type="application/json")
+        response.status_code = 400
+        response.content = json.dumps({"error": "Invalid Request"})
+        return response
+
+    def post(self, request):
+        response = HttpResponse(content_type="application/json")
+        email = request.POST.get("email", "")
+        username = email[:email.find("@")]
+        password = request.POST.get("password")
+        print(email, password)
+
+        if email and password:
+            (user, created) = User.objects.get_or_create(username=username)
+            print(created)
+
+            if created: #email = unique
+                # create user
+                user.set_password(password)
+                user.email = email
+                user.save()
+
+                response.status_code = 200
+                response.content = json.dumps({"success": "user registerd"})
+            else:
+                response.status_code = 400
+                response.content = json.dumps({"error": "An account already exists with this email"})
+        else:
+            response.status_code = 400
+            response.content = json.dumps({"error": "Email and password are required for account creation"})
+        return response
+
+
+class SetUserDietaryPreferences(View):
+
+    def get(self, request):
+        response = HttpResponse(content_type="application/json")
+        response.status_code = 400
+        response.content = json.dumps({"error": "Invalid Request"})
+        return response
+
+    def post(self, request):
+        response = HttpResponse(content_type="application/json")
+        email = request.POST.get("email", "")
+        preferences = request.POST.get("preferences", "{}")
+        preferences = json.loads(preferences)
+
+        if email:
+            try:
+                user = User.objects.get(email=email)
+            except User.DoesNotExist:
+                user = None
+
+            if user:
+                try:
+                    user_preference = UserDietaryPreferences.objects.get(user=user)
+                except UserDietaryPreferences.DoesNotExist:
+                    user_preference = UserDietaryPreferences()
+                    user_preference.dietary_preferences = DiertaryRequirements()
+
+                for p in preferences.keys():
+                    if p == "has_halal" and preferences[p]==1:
+                        user_preference.dietary_preferences.has_halal=True
+                    elif p == "has_sanitary_products" and preferences[p]==1:
+                        user_preference.dietary_preferences.has_sanitary_products = True
+                    elif  p == "has_vegetarian" and preferences[p]==1:
+                        user_preference.dietary_preferences.has_vegetarian = True
+                    elif p == "has_vegan" and preferences[p] == 1:
+                        user_preference.dietary_preferences.has_vegan = True
+                    elif p == "has_kosher" and preferences[p] == 1:
+                        user_preference.dietary_preferences.has_kosher = True
+                    elif p == "has_gluten_free" and preferences[p] == 1:
+                        user_preference.dietary_preferences.has_gluten_free = True
+                    elif p == "has_lactose_free" and preferences[p] == 1:
+                        user_preference.dietary_preferences.has_lactose_free = True
+                    else:
+                        pass
+                user_preference.dietary_preferences.save()
+                user_preference.user = user
+                user_preference.save()
+
+                response.status_code = 200
+                response.content = json.dumps({"success": "preferences set successfully"})
+            else:
+                response.status_code = 400
+                response.content = json.dumps({"error": "user doesn't exist"})
+        else:
+            response.status_code = 400
+            response.content = json.dumps({"error": "No user specified"})
+        return response
+
+class EditUserProfile(View):
+
+    def get(self, request):
+        response = HttpResponse(content_type="application/json")
+        response.status_code = 400
+        response.content = json.dumps({"error": "Invalid Request"})
+        return response
+
+    def post(self, request):
+        response = HttpResponse(content_type="application/json")
+        email = request.POST.get("email", "")
+        new_email = request.POST.get("new_email", "")
+        new_pword = request.POST.get("new_pword", "")
+
+        preferences = json.loads(request.POST.get("new_preferences", ""))
+
+        if email:
+
+            try:
+                user = User.objects.get(email=email)
+            except User.DoesNotExist:
+                user = None
+
+            if user:
+                # update user
+                if new_email:
+                    user.email = new_email
+                if new_pword:
+                    user.set_password(new_pword)
+
+                user_pref = UserDietaryPreferences.objects.get(user=user)
+                diet = user_pref.dietary_preferences
+                for p in preferences.keys():
+                    if p == "has_halal":
+                        if preferences[p]==1:
+                            diet.has_halal=True
+                        else:
+                            diet.has_halal=False
+                    elif p == "has_sanitary_products":
+                        if preferences[p]==1:
+                            diet.has_sanitary_products = True
+                        else:
+                            diet.has_sanitary_products = False
+                    elif p == "has_vegetarian":
+                        if preferences[p]==1:
+                            diet.has_vegetarian = True
+                        else:
+                            diet.has_vegetarian = False
+                    elif p == "has_vegan":
+                        if preferences[p] == 1:
+                            diet.has_vegan = True
+                        else:
+                            diet.has_vegan = False
+                    elif p == "has_kosher":
+                        if preferences[p] == 1:
+                            diet.has_kosher = True
+                        else:
+                            diet.has_kosher = False
+                    elif p == "has_gluten_free":
+                        if preferences[p] == 1:
+                            diet.has_gluten_free = True
+                        else:
+                            diet.has_gluten_free = False
+                    elif p == "has_lactose_free":
+                        if preferences[p] == 1:
+                            diet.has_lactose_free = True
+                        else:
+                            diet.has_lactose_free = False
+                    else:
+                        pass
+                diet.save()
+
+                try:
+                    user.save()
+                    response.status_code = 200
+                    response.content = json.dumps({"success": "profile updated" })
+                except IntegrityError:
+                    response.status_code=400
+                    response.content=json.dumps({"error": "email already exist"})
+
+
+
+            else:
+                response.status_code = 400
+                response.content = json.dumps({"error": "user doesn't exist"})
+        else:
+            response.status_code = 400
+            response.content = json.dumps({"error": "No user specified"})
+        return response
+
+
+
+
+class SingleSignOn(View):
+
+    def get(self, request):
+        response = HttpResponse(content_type="application/json")
+        response.status_code = 400
+        response.content = json.dumps({"error": "Invalid Request"})
+        return response
+
+    def post(self, request):
+        response = HttpResponse(content_type="application/json")
+        email =request.POST.get("email", "")
+        print(type(email), email)
+
+        if email:
+            import random, string
+            (user, created) = User.objects.get_or_create(email=email)
+            random_pword = "".join(random.sample(string.ascii_letters + string.digits + string.punctuation, 8))
+            send_mail("Foodbank: temporary password",
+                      f"Please login in with this password: \n {random_pword} \n.",
+                      EMAIL_HOST_USER,
+                      [email],
+                      fail_silently=False)
+            if created:
+                user.username=email[:email.find("@")]
+            user.set_password(random_pword)
+            user.save()
+            response.status_code = 200
+            response.content=json.dumps({"success": "email sent"})
+
+        else:
+            response.status_code = 400
+            response.content = json.dumps({"error": "No email entered"})
+        return response
+
+class UserLogin(View):
+
+    def get(self, request):
+        response = HttpResponse(content_type="application/json")
+        response.status_code = 400
+        response.content = json.dumps({"error": "Invalid Request"})
+        return response
+
+    def post(self, request):
+        response = HttpResponse(content_type="application/json")
+        email =request.POST.get("email", "")
+        pword = request.POST.get("password", "")
+
+        if email and pword:
+            try:
+                user = User.objects.get(email=email)
+                if user.check_password(pword):
+                    response.status_code = 200
+                    response.content=json.dumps({'status': 'login success'})
+                else:
+                    response.status_code = 400
+                    response.content=json.dumps({'error': 'wrong X email or password'})
+            except User.DoesNotExist:
+                response.status_code = 400
+                response.content=json.dumps({'error': 'wrong email or password'})
+
+        else:
+            response.status_code = 400
+            response.content = json.dumps({"error": "no username or password entered"})
+        return response            
 
 class TestEmail(View):
 
